421b56d163a058a6eca897fb9f682c2e7cd14624
sachin <sk800968@gmail.com>
2025-05-29 15:55:57 +0530

    added city-controller and router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
-
 const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
 const setupAndStartServer = async () => {
-
   //create the express
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.listen(PORT, async() => {
+  app.use('/api' , ApiRoutes);
+  
+  app.listen(PORT, async () => {
     console.log(`server started at ${PORT}`);
   });
-  
 };
 
 setupAndStartServer();
